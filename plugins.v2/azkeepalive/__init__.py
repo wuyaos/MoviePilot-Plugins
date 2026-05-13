ac7c59fa4708735a19469f1b5b4afe1e380cad33
wuyaos <wufeifeng_hust@163.com>
2026-05-13 14:43:01 +0800

    Bump AzKeepAlive to v2.2.0

--- plugins.v2/azkeepalive/__init__.py
+++ plugins.v2/azkeepalive/__init__.py
@@ -18,7 +18,7 @@ class AzKeepAlive(_PluginBase):
     plugin_name = "AnimeZ保活"
     plugin_desc = "定时访问AnimeZ站点并从种子页选种提交下载器，满足保活要求"
     plugin_icon = "https://raw.githubusercontent.com/wuyaos/MoviePilot-Plugins/main/icons/refresh.png"
-    plugin_version = "2.1.0"
+    plugin_version = "2.2.0"
     plugin_author = "wuyaos"
     author_url = "https://github.com/wuyaos"
     plugin_config_prefix = "azkeepalive_"
